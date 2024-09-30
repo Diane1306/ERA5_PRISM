e4ddf83a0666ea7d7dea6ed5f08c984d891a5f6c
diane_wt <dianewang06@gmail.com>
2024-09-29 22:42:56 -0700

    update

--- 21Fig8_FalseSpringIndex.py
+++ 21Fig8_FalseSpringIndex.py
@@ -35,7 +35,7 @@ ni = 1./3.
 fsi = np.where(np.logical_and(sddn<ni, sdd<fdd1), 1, 0)
 fsi1 = np.where(mask, fsi, np.nan)
 
-fdd3 = np.load('./var/Cherry_FirstDamDate3days.npy')
+fdd3 = np.load(work_dir + 'var/Cherry_FirstDamDate3days.npy')
 fsi = np.where(np.logical_and(sddn<ni, sdd<fdd3), 1, 0)
 fsi3 = np.where(mask, fsi, np.nan)
 
@@ -45,7 +45,7 @@ extent = [-105, -75, 34, 49]
 fsidata = [fsi1, fsi3]
 title = ['(a) 1 damage days after side green date', '(b) 3 damage days after side green date', '(c) Skewness of Side Green Day']
 yr = 16
-fig, axes = plt.subplots(3, 1, figsize=(12, 8))
+fig, axes = plt.subplots(3, 1, figsize=(5, 10))
 pc = []
 for i in range(3):
     ax = plt.subplot(3, 1, i+1, projection=ccrs.AlbersEqualArea(np.mean(extent[:2]), np.mean(extent[2:])))
@@ -63,20 +63,29 @@ for i in range(3):
         pc.append(plt.pcolormesh(Lon, Lat, fsidata[i].sum(axis=0), cmap=cmap, norm=norm, transform=ccrs.PlateCarree()))
     else:
         levels = MaxNLocator(nbins=100).tick_values(-1, 1)
-        cmap = plt.get_cmap('plasma_r')
+        cmap = plt.get_cmap('bwr')
         norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
         pc.append(plt.pcolormesh(Lon, Lat, skewness, cmap=cmap, norm=norm, transform=ccrs.PlateCarree()))
 
     plt.title(f'{title[i]}', fontsize=16, fontweight='bold')
 
+for ax in axes.flat:  # Loop through all axes objects
+    ax.set_xticks([])  # Remove x-axis ticks
+    ax.set_yticks([])
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(True)  # Ensure only the bottom spine is visible
+
 plt.subplots_adjust(bottom=0.02, top=.98, left=0.02, right=.98, wspace=0.005, hspace=0.1)
 i = 0
-cb_ax = fig.add_axes([0.985, 0.345, 0.01, 0.64])
+cb_ax = fig.add_axes([0.985, 0.345, 0.02, 0.64])
 cbar = fig.colorbar(pc[i], cax=cb_ax, ticks=np.arange(0, yr+1, 2), extend='max')
 cb_ax.tick_params(labelsize=15)
 cb_ax.tick_params(axis='y', which='minor', length=0)  # Remove minor ticks
 i = 2
-cb_ax = fig.add_axes([0.985, .02, 0.01, 0.32])
+cb_ax = fig.add_axes([0.985, .02, 0.02, 0.31])
 cbar = fig.colorbar(pc[i], cax=cb_ax, ticks=np.arange(-1, 1.1, .2), extend='both')
 cb_ax.tick_params(labelsize=15)
 cb_ax.tick_params(axis='y', which='minor', length=0)  # Remove minor ticks
